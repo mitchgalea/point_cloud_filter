74c216777731cc7ba05b215d4c6cbbfac1aeb590
mitch <mitchell_galea@hotmail.com>
2019-02-27 10:28:18 +1100

    added noise

--- src/point_cloud_filter_node.cpp
+++ src/point_cloud_filter_node.cpp
@@ -15,6 +15,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
+#include <random>
+#include <math.h>
 
 class PointCloudFilterNode{
 
@@ -25,6 +28,7 @@ class PointCloudFilterNode{
     double lower_limit_;
     double upper_limit_;
     double bracket_;
+    double z_cutoff_;
 
 
 
@@ -36,7 +40,8 @@ public:
         pc_pub_ = nh_.advertise<pcl::PointCloud<pcl::PointXYZRGB> > ("/camera/depth/points/filtered", 100);
         
         ros::NodeHandle pn("~");
-        pn.param<double>("bracket", bracket_, 0.01);
+        pn.param<double>("bracket", bracket_, 0.001);
+        pn.param<double>("z_cutoff", bracket_, 4.00);
 
         tf2_ros::Buffer tfBuffer;
         tf2_ros::TransformListener tfListener(tfBuffer);
@@ -58,15 +63,22 @@ public:
             }
             if(listened)
             {
-                lower_limit_ = transformStamped.transform.translation.y - bracket_;
-                upper_limit_ = transformStamped.transform.translation.y + bracket_;
+                lower_limit_ = transformStamped.transform.translation.y - 0.001;
+                upper_limit_ = transformStamped.transform.translation.y + 0.001;
             }
         }
-
-
-
     }
 
+    double noise(double z)
+    {
+        double std_dev = 0.004 * pow(z, 1.5);
+        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+        std::default_random_engine generator (seed);
+        std::normal_distribution<double> normal_distribution(0.0, std_dev);
+
+        double n = normal_distribution(generator);
+
+    }
 
     void pcCallback(const sensor_msgs::PointCloud2ConstPtr& pCloud)
     {
@@ -81,13 +93,17 @@ public:
         y_pass.setFilterFieldName ("y");
         y_pass.setFilterLimits (lower_limit_, upper_limit_);
         y_pass.filter (*cloud_y_filtered);
+
+        for(size_t i = 0; i < cloud_y_filtered->points.size(); i++)
+        {
+            cloud_y_filtered->points[i].z += noise(cloud_y_filtered->points[i].z);
+        }
         
         pcl::PassThrough<pcl::PointXYZRGB> z_pass;
         z_pass.setInputCloud (cloud_y_filtered);
         z_pass.setFilterFieldName ("z");
-        z_pass.setFilterLimits (0.1, 1.0);
+        z_pass.setFilterLimits (0.1, 3.0);
         z_pass.filter (*cloud_z_filtered);
-
         pc_pub_.publish(*cloud_z_filtered);
 
     }
